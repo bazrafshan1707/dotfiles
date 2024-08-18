c3ec7ae0c5280b5e844d8b3d9ed981945fd35c5f
Ali Bazrafshan <ali@bazrafshan.net>
2024-08-18 17:49:58 +0330

    InstallUsefulApps: Update scoop if it's already installed
    Install scoop if it's not installed, and update it if it is.

--- .local/bin/InstallUsefulApps.ps1
+++ .local/bin/InstallUsefulApps.ps1
@@ -1,3 +1,8 @@
 # Install Scoop
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
+	Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+	Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+}
+else {
+	scoop update
+}
